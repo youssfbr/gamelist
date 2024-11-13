361fa098e9676db80d63bce5be7e50cf540a6b40
Alisson Youssf <youssfbr@gmail.com>
2024-11-13 19:01:36 -0300

    Fix: Game entity, Game seed

--- src/main/java/com/github/youssfbr/gamelist/entities/Game.java
+++ src/main/java/com/github/youssfbr/gamelist/entities/Game.java
@@ -5,11 +5,11 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "tb_games")
+@Table(name = "tb_game")
 public class Game {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
@@ -18,21 +18,22 @@ public class Game {
     private Integer year;
 
     private String genre;
-    private String platform;
+    private String platforms;
     private Double score;
     private String imgUrl;
     private String shortDescription;
+    @Column(columnDefinition = "text")
     private String longDescription;
 
     public Game() {}
 
-    public Game(Long id , String title , Integer year , String genre , String platform , Double score , String imgUrl ,
+    public Game(Long id , String title , Integer year , String genre , String platforms , Double score , String imgUrl ,
                 String shortDescription , String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -71,12 +72,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public Double getScore() {
